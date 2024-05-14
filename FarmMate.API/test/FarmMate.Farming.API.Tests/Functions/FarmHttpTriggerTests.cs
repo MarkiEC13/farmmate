ca7a3415b93d5bdda62e23af44d9ddaa83d57673
Mark Fernando <markchrishantha13@gmail.com>
2024-05-14 20:50:31 +0200

    Added farm function

--- FarmMate.API/test/FarmMate.Farming.API.Tests/Functions/FarmHttpTriggerTests.cs
+++ FarmMate.API/test/FarmMate.Farming.API.Tests/Functions/FarmHttpTriggerTests.cs
@@ -1,6 +1,10 @@
+using Azure.Core.Serialization;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Moq;
+using TestProject1.Mocks;
 
 namespace TestProject1.Functions;
 
@@ -9,4 +13,16 @@ public class FarmHttpTriggerTests
     private readonly Mock<FunctionContext> _functionContext;
     private readonly HttpRequestData _request;
 
+    public FarmHttpTriggerTests()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton(Options.Create(new WorkerOptions{Serializer = new JsonObjectSerializer()}));
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        _functionContext = new Mock<FunctionContext>();
+        _functionContext.Setup(i => i.InstanceServices)
+            .Returns(serviceProvider);
+        
+        _request = FakeHttpRequestDataExtensions.GetRequestData(String.Empty);
+    }
 }
