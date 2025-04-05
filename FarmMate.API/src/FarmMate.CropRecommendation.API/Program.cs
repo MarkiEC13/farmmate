12a0bc4445a283a5c5cf2992a89d57373595e11a
Mark Fernando <markchrishantha13@gmail.com>
2025-04-05 15:11:04 +0200

    Removed invalid keys

--- FarmMate.API/src/FarmMate.CropRecommendation.API/Program.cs
+++ FarmMate.API/src/FarmMate.CropRecommendation.API/Program.cs
@@ -12,7 +12,7 @@ using OpenAI.Extensions;
 using OpenAI.ObjectModels;
 
 var host = new HostBuilder()
-    .ConfigureAppConfiguration(configBuilder => 
+    .ConfigureAppConfiguration(configBuilder =>
         configBuilder
             .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
             .AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true)
@@ -23,12 +23,13 @@ var host = new HostBuilder()
             builder.UseMiddleware<GlobalExceptionHandlerMiddleware>();
         }
     )
-    .ConfigureServices(services => {
+    .ConfigureServices(services =>
+    {
         services.Configure<JsonSerializerOptions>(options =>
         {
             options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
         });
-        
+
         services.AddSingleton<IOpenApiConfigurationOptions>(_ => new OpenApiConfigurationOptions
         {
             Info = new OpenApiInfo
@@ -42,14 +43,14 @@ var host = new HostBuilder()
             ForceHttps = false,
             ForceHttp = false,
         });
-        
+
         services
             .AddApplicationInsightsTelemetryWorkerService()
             .ConfigureFunctionsApplicationInsights();
-        
-        services.AddOpenAIService(settings =>
+
+        _ = services.AddOpenAIService(settings =>
         {
-            settings.ApiKey = Environment.GetEnvironmentVariable("OpenAIServiceOptions__ApiKey") ?? "sk-proj-ilJ3zuBlJ3p371qqhxHaT3BlbkFJh5jHNLQA2jsn50X5p4LM";
+            settings.ApiKey = Environment.GetEnvironmentVariable("OpenAIServiceOptions__ApiKey") ?? throw new ArgumentNullException("OpenAI API key required");
             settings.DefaultModelId = Models.Gpt_4_turbo_preview;
         });
     })
